fdbc9193f1a37a944d9ba1433be568a26320ffbd
Ravindu Lakshitha <ravindulakshitha250@gmail.com>
2025-01-11 17:08:36 +0530

    finalize the navbar and add router for navbar

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Navbar.css"; // Add your styles here
+
+const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-logo">
+          LMS
+        </a>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className={isMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a href="/" className="nav-links">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/dashboard" className="nav-links">
+              Dash Board
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-links">
+            Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/login" className="nav-links">
+              Login
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
