5270da297a079d31ac09ef77142184b984435f83
Ravindu Lakshitha <ravindulakshitha250@gmail.com>
2025-01-13 18:47:56 +0530

    Created show books in hero page

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,52 @@
 import "./Hero.css";
-// import Heroimg from "../assets/Hero.jpg";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Book {
+  id: string;
+  name: string;
+  author: string;
+  description: string;
+}
 
 const Hero = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+
+  // Fetch book data using axios
+  useEffect(() => {
+    axios
+      .get("https://localhost:7086/api/Books") 
+      .then((response) => {
+        setBooks(response.data);
+
+      })
+      .catch(() => {
+        setError("Failed to fetch books");
+      });
+  }, []);
+
   return (
     <div className="hero">
+      <div className="font">
       <div className="hero__content">
       <p>Where Stories Meet Simplicity</p>
       <h1>Seamlessly<br />organize and access your collection with ease.</h1> 
       </div>
+      </div>
+      
+      {error && <div className="error">{error}</div>}
+      <div className="Read_book">
+  {books.map((book) => (
+    <div className="card" key={book.id}>
+      <div className="name">{book.name}</div>
+      <div className="Author">{book.author}</div>
+      <div className="Desc">{book.description}</div>
     </div>
+  ))}
+</div>
+      </div>
   );
 };
 
